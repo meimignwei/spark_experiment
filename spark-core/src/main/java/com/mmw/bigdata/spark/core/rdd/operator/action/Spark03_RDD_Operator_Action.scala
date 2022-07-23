d41897e05691c58c832181e049e87b87bf7d54c3
mmw <mingweimei@tencent.com>
2022-07-23 19:54:30 +0800

    行动算子

---
+++ spark-core/src/main/java/com/mmw/bigdata/spark/core/rdd/operator/action/Spark03_RDD_Operator_Action.scala
@@ -0,0 +1,28 @@
+package com.mmw.bigdata.spark.core.rdd.operator.action
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark03_RDD_Operator_Action {
+
+    def main(args: Array[String]): Unit = {
+
+        val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+        val sc = new SparkContext(sparkConf)
+
+        val rdd = sc.makeRDD(List(1,2,3,4),2)
+
+        // TODO - 行动算子
+
+        //10 + 13 + 17 = 40
+        // aggregateByKey : 初始值只会参与分区内计算
+        // aggregate : 初始值会参与分区内计算,并且和参与分区间计算
+        //val result = rdd.aggregate(10)(_+_, _+_)
+        //分区内和分区间规则相同时，可以使用fold
+        val result = rdd.fold(0)(_+_)
+
+        println(result)
+
+        sc.stop()
+
+    }
+}
